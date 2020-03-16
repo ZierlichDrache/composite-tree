1d32c0109a75bbe98391742131440dc11db19833
zierlich.drache@gmail.com <zierlich.drache@gmail.com>
2020-03-16 21:14:49 +0100

    Added generated spring boot project

---
+++ src/main/java/pl/solejnik/compositetree/CompositeTreeApplication.java
@@ -0,0 +1,13 @@
+package pl.solejnik.compositetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/solejnik/compositetree/CompositeTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.solejnik.compositetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
